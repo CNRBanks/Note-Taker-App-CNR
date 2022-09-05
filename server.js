dda85cac5d7343adabcd0b7bde413b6648b40956
CNRBanks <connortamminen@gmail.com>
2022-09-05 12:15:36 -0500

    setup html requests

--- server.js
+++ server.js
@@ -4,34 +4,30 @@ const path = require("path");
 // setup asynchronous processes?
 // setup server
 const app = express();
-const PORT = 3001;
+const PORT = 3006;
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api", api);
+// app.use("/api", api);
 app.use(express.static("./develop/public"));
 
-// setup GET Request
-// setup POST Request
-// setup DELETE Request
+// setup GET Request - notes.html
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
+);
+// setup GET Request - index.html
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+);
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
+);
+// setup GET Request - API/Notes
+
+// setup POST Request - API/Notes - new note to save on the request body
+// setup DELETE Request *BONUS*
 // setup HTML routes for both  index and notes files
-
-// GIVEN a note-taking application
-
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
-
-// WHEN I click on the link to the notes page
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
-
-// WHEN I enter a new note title and the note’s text
-// THEN a Save icon appears in the navigation at the top of the page
-
-// WHEN I click on the Save icon
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
-
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+// listening for port
+app.listen(PORT, () =>
+  console.log(`Note-Taker app listening at http://localhost:${PORT}`)
+);
